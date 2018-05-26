5fbc4952aecad607a8da6b96daa5a4085b711438
boa2017 <beatrizovejero@gmail.com>
2018-05-26 16:56:38 +0200

    Copyright

--- app/src/main/java/com/example/android/tourguide/Word.java
+++ app/src/main/java/com/example/android/tourguide/Word.java
@@ -1,8 +1,49 @@
+/*
+ *
+ *  * PROJECT LICENSE
+ *  *
+ *  * This project was submitted by Beatriz Ovejero as part of the Android Developer
+ *  * Nanodegree at Udacity.
+ *  *
+ *  * As part of Udacity Honor code, your submissions must be your own work, hence
+ *  * submitting this project as yours will cause you to break the Udacity Honor Code
+ *  * and the suspension of your account.
+ *  *
+ *  * As author of the project, I allow you to check it as a reference, but if you submit it
+ *  * as your own project, it's your own responsibility if you get expelled.
+ *  *
+ *  * Copyright (c) 2018 Beatriz Ovejero
+ *  *
+ *  * Besides the above notice, the following license applies and this license notice must be
+ *  * included in all works derived from this project.
+ *  *
+ *  * MIT License
+ *  *
+ *  * Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  * of this software and associated documentation files (the "Software"), to deal
+ *  * in the Software without restriction, including without limitation the rights
+ *  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  * copies of the Software, and to permit persons to whom the Software is
+ *  * furnished to do so, subject to the following conditions:
+ *  *
+ *  * The above copyright notice and this permission notice shall be included in all
+ *  * copies or substantial portions of the Software.
+ *  *
+ *  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  * SOFTWARE.
+ *
+ */
+
 package com.example.android.tourguide;
 
 /**
  * {@link Word} represents a vocabulary word that the user wants to learn.
- * It contains resource IDs for the default translation, Miwok translation, audio file, and
+ * It contains resource IDs for the common name, scientific name, and
  * optional image file for that word.
  */
 public class Word {
@@ -12,11 +53,11 @@ public class Word {
      */
     private static final int NO_IMAGE_PROVIDED = -1;
     /**
-     * String resource ID for the default translation of the word
+     * String resource ID for the common name of the word
      */
     private int mCommonNameId;
     /**
-     * String resource ID for the Miwok translation of the word
+     * String resource ID for the scientific name of the word
      */
     private int mScientificNameId;
     /**
@@ -39,14 +80,14 @@ public class Word {
     }
 
     /**
-     * Get the string resource ID for the default translation of the word.
+     * Get the string resource ID for the common name of the word.
      */
     public int getCommonNameId() {
         return mCommonNameId;
     }
 
     /**
-     * Get the string resource ID for the Miwok translation of the word.
+     * Get the string resource ID for the scientific name of the word.
      */
     public int getScientificNameId() {
         return mScientificNameId;
--- app/src/main/java/com/example/android/tourguide/WordAdapter.java
+++ app/src/main/java/com/example/android/tourguide/WordAdapter.java
@@ -1,3 +1,44 @@
+/*
+ *
+ *  * PROJECT LICENSE
+ *  *
+ *  * This project was submitted by Beatriz Ovejero as part of the Android Developer
+ *  * Nanodegree at Udacity.
+ *  *
+ *  * As part of Udacity Honor code, your submissions must be your own work, hence
+ *  * submitting this project as yours will cause you to break the Udacity Honor Code
+ *  * and the suspension of your account.
+ *  *
+ *  * As author of the project, I allow you to check it as a reference, but if you submit it
+ *  * as your own project, it's your own responsibility if you get expelled.
+ *  *
+ *  * Copyright (c) 2018 Beatriz Ovejero
+ *  *
+ *  * Besides the above notice, the following license applies and this license notice must be
+ *  * included in all works derived from this project.
+ *  *
+ *  * MIT License
+ *  *
+ *  * Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  * of this software and associated documentation files (the "Software"), to deal
+ *  * in the Software without restriction, including without limitation the rights
+ *  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  * copies of the Software, and to permit persons to whom the Software is
+ *  * furnished to do so, subject to the following conditions:
+ *  *
+ *  * The above copyright notice and this permission notice shall be included in all
+ *  * copies or substantial portions of the Software.
+ *  *
+ *  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  * SOFTWARE.
+ *
+ */
+
 package com.example.android.tourguide;
 
 import android.content.Context;
@@ -48,14 +89,14 @@ public class WordAdapter extends ArrayAdapter<Word> {
 
         // Find the TextView in the list_item.xml layout with the ID scientific_text_view.
         TextView scientificnameTextView = (TextView) listItemView.findViewById(R.id.scientificname_text_view);
-        // Get the Miwok translation from the currentWord object and set this text on
-        // the Miwok TextView.
+        // Get the scientific name from the currentWord object and set this text on
+        // the scientificname TextView.
         scientificnameTextView.setText(currentWord.getScientificNameId());
 
         // Find the TextView in the list_item.xml layout with the ID commonname_text_view.
         TextView commonnameTextView = (TextView) listItemView.findViewById(R.id.commonname_text_view);
-        // Get the default translation from the currentWord object and set this text on
-        // the default TextView.
+        // Get the common name from the currentWord object and set this text on
+        // the common name TextView.
         commonnameTextView.setText(currentWord.getCommonNameId());
 
         // Find the ImageView in the list_item.xml layout with the ID image.
